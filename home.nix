af4ae9a8c74a591ce53b68c8f2030b909c09f609
ztk37 <ztk37.contact@gmail.com>
2022-07-30 15:06:36 +0200

    update: add config.nix and extend home.nix

--- home.nix
+++ home.nix
@@ -1,35 +1,54 @@
 # Docs: https://rycee.gitlab.io/home-manager/options.html
 
-{ config, pkgs, ... }:
+{ pkgs, ... }: {
+  # imports = [ ./modules/common.nix ];
 
-{
   home = {
-    # Home Manager needs a bit of information about you and the
-    # paths it should manage.
     username = "cedric";
     homeDirectory = "/Users/cedric";
-
-    # file.".gitconfig".source = ./gitconfig;
-
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
     stateVersion = "22.05";
 
     packages = [
       # Development
       pkgs.git
-      # pkgs.vscode
+      pkgs.hub
+      pkgs.vscode
+
+      # Nodejs
+      pkgs.nodejs
+
+      # Nix
+      pkgs.niv
+      pkgs.nixfmt
+      pkgs.lorri
+
+      # Haskell
+      pkgs.hlint
+      # pkgs.ghcup
+      pkgs.ghcid
+
+      # Elixir/Erlang
+      pkgs.elixir
+      pkgs.erlang
+      pkgs.rebar3
+
+      # Rust
+      pkgs.rustup
+      # pkgs.rustfmt
+      # pkgs.rust-make
+
+      # Python
+      # Go
+      pkgs.go
+      #Swift
 
       # Misc
-      pkgs.coreutils
-      pkgs.ranger
       pkgs.jq
+
+      # Why not?
+      # pkgs.prolog
+      # pkgs.lisp
+      # pkgs.agda
     ];
   };
 
@@ -74,7 +93,7 @@
         free = "free -h";
 
         mkdir = "mkdir -p";
-        ls = "ls --color=auto --group-directories-first";
+        # ls = "ls --color=auto --group-directories-first";
         ll = "ls -l";
         la = "ls -a";
         l = "ls -la";
@@ -91,9 +110,21 @@
       enable = true;
 
       extraConfig = ''
-        set number
+        set number;
         set noswapfile
       '';
+    };
+
+    vscode = {
+      enable = true;
+      package = pkgs.vscode;
+      extensions = with pkgs.vscode-extensions; [
+        justusadam.language-haskell
+        golang.go
+      ];
+      userSettings = {
+        # "terminal.integrated.fontFamily" = "Hack";
+      };
     };
 
     jq.enable = true;
